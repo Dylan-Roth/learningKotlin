ca2d924e443148f5615704d8615b0e3f5c8d546b
Dylan Roth <droth@schoology.com>
2018-03-29 10:18:45 -0400

    hw2_classes Initialized parameters in Teacher class

--- src/Teacher.kt
+++ src/Teacher.kt
@@ -1,9 +1,13 @@
-class Teacher {
-    var firstName: String
-    var lastName: String
-    var age: Int
-    var gender: String
-    var currentSubject: String
-    val students = arrayOf("Sample")
-    fun studentWithHighestGrade()
+class Teacher constructor(name: String) {
+    init {
+        var firstName: String
+        var lastName: String
+        var age: Int
+        var gender: String
+        var currentSubject: String
+        val students = arrayOf("Sample")
+        fun studentWithHighestGrade(){
+            print("Placeholder")
+        }
+    }
 }
