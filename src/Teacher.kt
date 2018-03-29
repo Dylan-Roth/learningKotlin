532ddbef39630c17cda886096d5f0b9ee44d1116
Dylan Roth <droth@schoology.com>
2018-03-29 10:07:09 -0400

    hw2_classes Add properties to Teacher.kt

--- src/Teacher.kt
+++ src/Teacher.kt
@@ -1,2 +1,9 @@
 class Teacher {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    var gender: String
+    var currentSubject: String
+    val students = arrayOf("Sample")
+    fun studentWithHighestGrade()
 }
